1e229f7e5b7b8babb38524b2a4dfb6b74cc4cde4
ledinhtien219 <51738135+ledinhtien219@users.noreply.github.com>
2026-08-03 23:21:59 +0700

    Create lv_conf.h

---
+++ lv_conf.h
@@ -0,0 +1,17 @@
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+#define LV_COLOR_DEPTH 16
+#define LV_COLOR_16_SWAP 0
+
+#define LV_MEM_CUSTOM 0
+#define LV_MEM_SIZE (48U * 1024U)
+
+#define LV_USE_LOG 0
+#define LV_USE_ASSERT_NULL 0
+#define LV_USE_ASSERT_MALLOC 0
+#define LV_USE_ASSERT_STYLE 0
+#define LV_USE_ASSERT_MEM_INTEGRITY 0
+#define LV_USE_ASSERT_OBJ 0
+
+#endif
